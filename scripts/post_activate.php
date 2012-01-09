718f952366d0774731b54e4bbea4f705361b9504
royganor <roy@zend.com>
2012-01-09 09:13:25 +0200

    first contribution facebook-app

---
+++ scripts/post_activate.php
@@ -0,0 +1,9 @@
+<?php
+/* The script post_stage.php allow users to "clean up" after a new version of the application
+ * is activated, for example by removing a "Down for Maintenance" message
+ * set in Pre-activate.
+ */  
+// get application location
+$appLocation = getenv('ZS_APPLICATION_BASE_DIR');
+file_put_contents($appLocation . '/config/app.ini', 'appid=' . getenv('ZS_APPID'));
+
