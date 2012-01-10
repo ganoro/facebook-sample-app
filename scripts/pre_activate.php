2c511ccf86fc33c6cdd11a4a5420adcab88f4f6b
royganor <roy@zend.com>
2012-01-10 14:57:33 +0200

    add pre_activate script

---
+++ scripts/pre_activate.php
@@ -0,0 +1,7 @@
+<?php
+/* The script pre_activate.php should contain code that should make the changes in the server
+ * environment so that the application is fully functional. For example, this may include
+ * changing symbolic links to "data" directories from previous to current versions,
+ * upgrading an existing DB schema, or setting up a "Down for Maintenance"
+ * message on the live version of the application
+ */  
